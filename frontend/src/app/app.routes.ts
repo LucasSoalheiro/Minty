ce036503488c20bb2fb1011a34950b4266e7d7e8
GustavoTorelli <gustavo.torelli17@gmail.com>
2026-08-17 21:59:10 -0300

    feat: New images, and header init

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './layout/auth-layout/auth-layout';
+import { PublicLayout } from './layout/public-layout/public-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PublicLayout,
+  },
+  {
+    path: 'auth-login',
+    component: AuthLayout,
+  },
+];
