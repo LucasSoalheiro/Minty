0966ec2616e3037e0b8fc2a6fe873d1c383a69ef
GustavoTorelli <gustavo.torelli17@gmail.com>
2026-08-25 00:29:27 -0300

    feat: add NoPage component for handling 404 errors with custom layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthLayout } from './layout/auth-layout/auth-layout';
 import { PublicLayout } from './layout/public-layout/public-layout';
+import { NoPage } from './layout/no-page/no-page';
 
 export const routes: Routes = [
   {
@@ -11,4 +12,8 @@ export const routes: Routes = [
     path: 'auth-login',
     component: AuthLayout,
   },
+  {
+    path:'**',
+    component: NoPage,
+  }
 ];
---
+++ frontend/src/app/layout/no-page/no-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-no-page',
+  styleUrl: './no-page.scss',
+  templateUrl: './no-page.html',
+})
+export class NoPage {}
